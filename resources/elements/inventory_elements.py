23a7f84dda45a860fadca1579e107e64c0edb9ad
Lucas Pereira <lucaspereiraribeirolpr@gmail.com>
2026-06-30 20:13:55 -0300

    feat(inventory): map products and cart selectors

---
+++ resources/elements/inventory_elements.py
@@ -0,0 +1,4 @@
+btn_add_backpack      = "css=[data-test='add-to-cart-sauce-labs-backpack']"
+btn_add_bike_light    = "css=[data-test='add-to-cart-sauce-labs-bike-light']"
+icon_cart             = "css=[data-test='shopping-cart-link']"
+badge_cart_counter    = "css=[data-test='shopping-cart-badge']"
