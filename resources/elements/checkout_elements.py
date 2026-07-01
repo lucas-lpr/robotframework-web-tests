a9edd4f56a3780d0f7c0a2e9aac63f201d32b4e4
Lucas Pereira <lucaspereiraribeirolpr@gmail.com>
2026-06-30 22:04:04 -0300

    feat(checkout): map cart summary and shipping form selectors

---
+++ resources/elements/checkout_elements.py
@@ -0,0 +1,9 @@
+btn_checkout          = "css=[data-test='checkout']"
+
+txt_first_name        = "id=first_name"
+txt_last_name         = "id=last_name"
+txt_postal_code       = "id=postal-code"
+btn_continue          = "css=[data-test='continue']"
+
+btn_finish            = "css=[data-test='finish']"
+lbl_success_header    = "css=[data-test='complete-header']"
